e601a65010e71dd913ba80e2ce063b3b74256e61
Josh-JJ <josh@empireone.com.au>
2016-06-02 20:00:45 +1000

    sequential program

---
+++ DigitalMusic/WpfApplication1/timefreq.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace WpfApplication1
+{
+    public class timefreq
+    {
+        public float[][] timeFreqData;
+        public int wSamp;
+        public Complex[] twiddles;
+
+        public timefreq(float[] x, int windowSamp)
+        {
+            int ii;
+            double pi = 3.14159265;
+            Complex i = Complex.ImaginaryOne;
+            this.wSamp = windowSamp;
+            twiddles = new Complex[wSamp];
+            for (ii = 0; ii < wSamp; ii++)
+            {
+                double a = 2 * pi * ii / (double)wSamp;
+                twiddles[ii] = Complex.Pow(Complex.Exp(-i), (float)a);
+            }
+
+            timeFreqData = new float[wSamp/2][];
+
+            int nearest = (int)Math.Ceiling((double)x.Length / (double)wSamp);
+            nearest = nearest * wSamp;
+
+            Complex[] compX = new Complex[nearest];
+            for (int kk = 0; kk < nearest; kk++)
+            {
+                if (kk < x.Length)
+                {
+                    compX[kk] = x[kk];
+                }
+                else
+                {
+                    compX[kk] = Complex.Zero;
+                }
+            }
+
+
+            int cols = 2 * nearest /wSamp;
+
+            for (int jj = 0; jj < wSamp / 2; jj++)
+            {
+                timeFreqData[jj] = new float[cols];
+            }
+
+            timeFreqData = stft(compX, wSamp);
+	
+        }
+
+        float[][] stft(Complex[] x, int wSamp)
+        {
+            int ii = 0;
+            int jj = 0;
+            int kk = 0;
+            int ll = 0;
+            int N = x.Length;
+            float fftMax = 0;
+            
+            float[][] Y = new float[wSamp / 2][];
+
+            for (ll = 0; ll < wSamp / 2; ll++)
+            {
+                Y[ll] = new float[2 * (int)Math.Floor((double)N / (double)wSamp)];
+            }
+            
+            Complex[] temp = new Complex[wSamp];
+            Complex[] tempFFT = new Complex[wSamp];
+
+            for (ii = 0; ii < 2 * Math.Floor((double)N / (double)wSamp) - 1; ii++)
+            {
+
+                for (jj = 0; jj < wSamp; jj++)
+                {
+                    temp[jj] = x[ii * (wSamp / 2) + jj];
+                }
+
+                tempFFT = fft(temp);
+
+                for (kk = 0; kk < wSamp / 2; kk++)
+                {
+                    Y[kk][ii] = (float)Complex.Abs(tempFFT[kk]);
+
+                    if (Y[kk][ii] > fftMax)
+                    {
+                        fftMax = Y[kk][ii];
+                    }
+                }
+
+
+            }
+
+            for (ii = 0; ii < 2 * Math.Floor((double)N / (double)wSamp) - 1; ii++)
+            {
+                for (kk = 0; kk < wSamp / 2; kk++)
+                {
+                    Y[kk][ii] /= fftMax;
+                }
+            }
+
+            return Y;
+        }
+
+        Complex[] fft(Complex[] x)
+        {
+            //int ii = 0;
+            //int kk = 0;
+            int N = x.Length;
+
+            Complex[] Y = new Complex[N];
+
+            // NEED TO MEMSET TO ZERO?
+
+            if (N == 1)
+            {
+                Y[0] = x[0];
+            }
+            else{
+
+                Complex[] E = new Complex[N/2];
+                Complex[] O = new Complex[N/2];
+                Complex[] even = new Complex[N/2];
+                Complex[] odd = new Complex[N/2];
+
+                for (int j = 0; j < N; j++)
+                {
+
+                    if (j % 2 == 0)
+                    {
+                        even[j / 2] = x[j];
+                    }
+                    if (j % 2 == 1)
+                    {
+                        odd[(j - 1) / 2] = x[j];
+                    }
+                }
+
+                E = fft(even);
+                O = fft(odd);
+
+                for (int j = 0; j < N; j++)
+                {
+                    Y[j] = E[(j % (N / 2))] + O[(j % (N / 2))] * twiddles[j * wSamp / N];
+                }
+            }
+
+           return Y;
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Sequential-music-analysis
+= CAB401 - Assignment
+
+Parallelisation of the Digital Music Analysis (C#) example program
+
+= To Compile and Run
+
+1. Download / Clone the Git repo
+2. Open the program from within Visual Studio
+3. Build the solution from the build toolbar menu
+4. Run the program by clicking start
